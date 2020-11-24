ff1b82c5cffcc31935078a37cd47d92313be12c3
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2020-11-24 19:36:23 +0300

    Update script.sh

--- tasks/05. Rise of fallen node/script.sh
+++ tasks/05. Rise of fallen node/script.sh
@@ -3,6 +3,6 @@
 i=0
 while :
 do
-  cqlsh 172.31.77.183 -e "insert into timur.TEST(id, name) values ($(( i)),  'name777');";
+  cqlsh 172.31.77.183 -e "insert into timur.task5(id, name) values ($(( i)),  'name777');";
   i=$i+1;
 done
